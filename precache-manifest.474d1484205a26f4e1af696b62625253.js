74a0eb86608bead5225e8fe30b77126195250d4d
danish0313 <danishoo.dk@gmail.com>
2020-04-03 22:44:14 +0200

    Updates

---
+++ precache-manifest.474d1484205a26f4e1af696b62625253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79310712e169a263919140320fd82fe2",
+    "url": "/Bussiness-Dashboard/index.html"
+  },
+  {
+    "revision": "b7d0c947e0a0d113b28c",
+    "url": "/Bussiness-Dashboard/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "2ab46455688cf085bbdc",
+    "url": "/Bussiness-Dashboard/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "b7d0c947e0a0d113b28c",
+    "url": "/Bussiness-Dashboard/static/js/2.9cc4df0f.chunk.js"
+  },
+  {
+    "revision": "2ab46455688cf085bbdc",
+    "url": "/Bussiness-Dashboard/static/js/main.27130de0.chunk.js"
+  },
+  {
+    "revision": "7c70709e6b9a16f7160b",
+    "url": "/Bussiness-Dashboard/static/js/runtime-main.1a1e7aec.js"
+  }
+]);
